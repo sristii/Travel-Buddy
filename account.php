636fb759a676e23f267abe7022d38df6dae4575f
tangs731 <144080109+tangs731@users.noreply.github.com>
2023-12-09 21:19:16 -0500

    Update account.php

--- account.php
+++ account.php
@@ -50,7 +50,6 @@
 <body>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
 </body>
