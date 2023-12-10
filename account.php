eb78f6444cdf1d53d8c4f89481b12a2d3758829c
tangs731 <144080109+tangs731@users.noreply.github.com>
2023-12-09 21:59:20 -0500

    Update account.php

--- account.php
+++ account.php
@@ -21,34 +21,39 @@
             die("Connection failed: " . $conn->connect_error);
         }
         $email = $_SESSION["username"];
-
         $sql = "SELECT * FROM users";
-
         $result = $conn->query($sql);
 
+        //printing out basic html first
+         echo "<h1 class=my-5>Here are your previous trips!</h1>";
+
         //check to see if the emails match
+        $count = 0;
         if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            if ($row["email"] == $email)
-                echo "Name: ". $row["first_name"]. " ".$row["last_name"]. " <br>Email: ". $row["email"]. "<br> Card: " . $row["card"]."<br>"; 
+            if ($row["email"] == $email){
+                echo "Name: ". $row["first_name"]. " ".$row["last_Name"]. " <br>Email: ". $row["email"]. "<br> Card: ". $row["card"]. "<br> Package: ". $row["package"]."<br><br>"; 
+                $count = $count+1;
+            }
         }
-    } else {
-        echo "You have no records with us. Plan a trip today!";
-    }
+        }
+       if($count==0){
+            echo "You have no records with us. Plan a trip today!<br>";
+        }
     ?>
  
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>Account</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+<br>
     <p>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
